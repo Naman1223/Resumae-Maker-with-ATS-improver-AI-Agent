a97437d19f9415514656fb1d1f1cb58cc24f830d
Namant1223 <namant1223@gmail.com>
2026-01-07 15:14:30 +0530

    origin

--- pdf_phraser.py
+++ pdf_phraser.py
@@ -4,14 +4,24 @@ import sys
 from langchain_google_genai import ChatGoogleGenerativeAI
 import pymupdf4llm
 import os
+from rich.console import Console
+from rich.markdown import Markdown
+
+
+
+
 def extract_text(pdf_path):
     md_text = pymupdf4llm.to_markdown(pdf_path) 
     return md_text
 
 pdf_path = r"C:\Projects\Resumae Maker with ATS improver AI Agent\Resume-Nitin.pdf"
 md_text = extract_text(pdf_path)
+load_dotenv()
 api_key = os.getenv("GEMINI_API_KEY")
-model= ChatGoogleGenerativeAI(model="gemini-2.5-flash-lite",temperature=0,api_key="AIzaSyCcVfz1oeYBgb8Rd4ZNpt5gvD4Mpx2EfrQ")
+if not api_key:
+    raise ValueError("GEMINI_API_KEY not found in environment variables")
+
+model= ChatGoogleGenerativeAI(model="gemini-2.5-flash",temperature=0,api_key=api_key)
 messages=[
     {"role":"system","content":"You are a text extractor. Extract the text from the given markdown text and convert it into clean markdown format with proper spacing and formatting."},
     {"role":"user","content":md_text}
@@ -19,15 +29,16 @@ messages=[
 response=model.invoke(messages)
 
 ## This part takes the JOB description as user input and comparest it with the Resume and gives the score based on a scale of 1 to 100
-model= ChatGoogleGenerativeAI(model="gemini-2.5-flash-lite",temperature=0,api_key="AIzaSyCcVfz1oeYBgb8Rd4ZNpt5gvD4Mpx2EfrQ")
+model= ChatGoogleGenerativeAI(model="gemini-2.5-flash",temperature=0,api_key=api_key)
 
 print("Enter text (Ctrl+D or Ctrl+Z to save):")
 job_description = sys.stdin.read()
 
 messages=[
-    {"role":"system","content":"You are a resume scorer. Score the resume based on the job description and give the score based on a scale of 1 to 100." + job_description},
+    {"role":"system","content":"You are a resume ATS scorer. Score the resume based on the job description and give the score based on a scale of 1 to 100 also give a seprate detailed section on how to fix the ATS score to make it better for the JOB." + job_description},
     {"role":"user","content":md_text}
 ]
 response_ats=model.invoke(messages)
-print(response_ats.content)
-
+console = Console()
+md = Markdown(response_ats.content)
+console.print(md)
